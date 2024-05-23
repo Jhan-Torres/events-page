f0aba300fed9d69e719b684d79349b984653c0f1
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-05-22 23:03:56 -0500

    not found page added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,12 @@
+<script setup>
+import GoBackButton from '@/components/GoBackButton.vue';
+</script>
+
+<template>
+  <div>
+    <h1>Page not found</h1>
+    <GoBackButton />
+  </div>
+</template>
+
+<style scoped></style>
