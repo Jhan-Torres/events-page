23fa4e50d3e52746ad66864ce6c4b797aaf65aa6
Jhan-Torres <jtorrese17_1@unc.edu.pe>
2024-05-30 14:46:55 -0500

    show loading var on events page

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp, reactive } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import 'nprogress/nprogress.css'
+//create a global (reactive) object to share across multiple components
 const GStore = reactive({ flashMessage: "" });
 
-//create a global (reactive) object to share across multiple components
 const app = createApp(App);
 
 app.use(router).provide("GStore", GStore);
